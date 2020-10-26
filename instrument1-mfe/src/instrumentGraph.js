89e48f7db94d7e124c2339c8a6b9f154736120ed
Osvald Frisk <osvald.frisk@gmail.com>
2020-10-26 13:21:14 +0100

    refactored mfe and instrument

--- instrument1-mfe/src/instrumentGraph.js
+++ instrument1-mfe/src/instrumentGraph.js
@@ -9,22 +9,11 @@ import autobahn from 'autobahn-browser';
 const initialState = {
   datasets: [
     {
-      label: 'My First dataset',
+      label: "Mock Data",
       fill: false,
       lineTension: 0.1,
       backgroundColor: 'rgba(75,192,192,0.4)',
       borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
       pointRadius: 1,
       pointHitRadius: 10,
       data: []
@@ -41,15 +30,6 @@ const initialOptions = {
             refresh: 1000,      // onRefresh callback will be called every 1000 ms
             delay: 1000,        // delay of 1000 ms, so upcoming values are known before plotting a line
             pause: false,       // chart is not paused
-            ttl: undefined,     // data will be automatically deleted as it disappears off the chart
-            // onRefresh: function(chart) {
-            //   chart.data.datasets.forEach(function(dataset) {
-            //     dataset.data.push({
-            //       x: Date.now(),
-            //       y: Math.random()
-            //     });
-            //   });
-            // },
           }
       }],
       yAxes: [{
@@ -62,30 +42,10 @@ const initialOptions = {
         }
     },
     animation: {
-        duration: 0
+        duration: 1
     }
-    // // Assume x axis is the realtime scale
-    // pan: {
-    //     enabled: true,    // Enable panning
-    //     mode: 'x',        // Allow panning in the x direction
-    //     rangeMin: {
-    //         x: null       // Min value of the delay option
-    //     },
-    //     rangeMax: {
-    //         x: null       // Max value of the delay option
-    //     }
-    // },
-    // zoom: {
-    //     enabled: true,    // Enable zooming
-    //     mode: 'x',        // Allow zooming in the x direction
-    //     rangeMin: {
-    //         x: null       // Min value of the duration option
-    //     },
-    //     rangeMax: {
-    //         x: null       // Max value of the duration option
-    //     }
-    // }
 };
+
 
 class Graph extends React.Component {
     constructor () {
@@ -99,20 +59,8 @@ class Graph extends React.Component {
     }
 
 
-    handlePauseToggle () {
-        var newStateOptions = this.state.graphOptions
-        newStateOptions.scales.xAxes[0].realtime.pause = !newStateOptions.scales.xAxes[0].realtime.pause
-        this.setState({graphOptions: newStateOptions})
-        }
 
-    handleAxisTypeToggle () {
-        var newStateOptions = this.state.graphOptions
-        newStateOptions.scales.yAxes[0].type = newStateOptions.scales.yAxes[0].type == "linear" ? "logarithmic" : "linear"
-        this.setState({graphOptions: newStateOptions})
-    }
-    
-	componentDidMount(){
-        
+    componentDidMount(){
         this.autobahnConnection = new autobahn.Connection({
             url: 'ws://localhost:8080/ws',
             realm: 'realm1'
@@ -120,26 +68,16 @@ class Graph extends React.Component {
 
         this.autobahnConnection.onopen = (session, details) => {
             session.subscribe("com.myapp.hello", (args) => {
-                // console.log('Received Message: ', args[0])
 
-                var oldDataSet = this.state.graphData.datasets[0];
-                var newDataSet = {
-                    ...oldDataSet
-                };
-                
-                var newData = [...newDataSet.data, 
+                var newDataSet = this.state.graphData.datasets[0];
+                newDataSet.data = [...newDataSet.data,
                     {
                         x: Date.now(),
                         y: args[1]
                     }
-                ];        
-                newDataSet.data = newData;
+                ];
 
-                var newState = {
-                    datasets: [newDataSet]
-                };
-
-                this.setState({graphData: newState});
+                this.setState({graphData: {datasets: [newDataSet]}});
                 
             });
         }
@@ -152,15 +90,27 @@ class Graph extends React.Component {
         this.autobahnConnection.close();
     }
 
-	render() {
-		return (
+    render() {
+        return (
             <div>
-			<Line data={this.state.graphData} options={this.state.graphOptions}/>
+            <Line data={this.state.graphData} options={this.state.graphOptions}/>
             <button onClick={this.handlePauseToggle}>Pause</button>
             <button onClick={this.handleAxisTypeToggle}>Axis Type</button>
             </div>
-		);
-	}
+        );
+    }
+    handlePauseToggle () {
+        var newStateOptions = this.state.graphOptions
+        newStateOptions.scales.xAxes[0].realtime.pause = !newStateOptions.scales.xAxes[0].realtime.pause
+        this.setState({graphOptions: newStateOptions})
+        }
+
+    handleAxisTypeToggle () {
+        var newStateOptions = this.state.graphOptions
+        newStateOptions.scales.yAxes[0].type = newStateOptions.scales.yAxes[0].type == "linear" ? "logarithmic" : "linear"
+        this.setState({graphOptions: newStateOptions})
+    }
+    
 }
 
 export default Graph
